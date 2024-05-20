63ffdfb4debb57210034234d5c269fb1ac590b85
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-05-20 18:46:11 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', require('./routes/routes'))
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
