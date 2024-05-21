9970912a088ca5485cf3132410be0232357ebdfe
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-05-21 22:46:00 +0700

    fix: cleanup

--- routes/routes.js
+++ routes/routes.js
@@ -2,24 +2,20 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const accessValidation = require("../middleware/accessValidation");
+const postController = require("../controller/postController");
 
 router.get("/", (req, res) => {
     res.status(200).json({
-        "message": "Welcome to Rejuvify API server. We recommend that you first register and login before accessing our endpoints."
+        "message": "Welcome to UlikBatik API server. We recommend that you first register and login before accessing our endpoints."
     })
 });
 
-// router.get('/register', (req, res) => {
-//     res.status(200).json({
-//         "status": true,
-//         "message": "Register page"
-//     })
-
-// })
-
-router.get('/users', accessValidation, auth.getUsers )
 
 router.post('/register', auth.register)
 router.post('/login', auth.login)
+router.get('/users', accessValidation, auth.getUsers )
+
+router.get('/posts', accessValidation, postController.getPosts)
+router.get('/posts/:userId', accessValidation, postController.getPostsByUser)
 
 module.exports = router;
