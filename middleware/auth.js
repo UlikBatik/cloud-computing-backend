c266192ca72a8ee0ad715c6c9ddc84a7d6a0730d
rayhaaanm <muzakomrayhan@gmail.com>
2024-05-27 00:30:10 +0700

    fix: code cleaning

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
-const crypto = require("crypto")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
