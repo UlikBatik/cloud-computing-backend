d4a3d412e0efc1758b8abbc72c7c8c4f2e7b6fc6
rayhaaanm <muzakomrayhan@gmail.com>
2024-05-27 00:30:52 +0700

    feat: modules images created

---
+++ modules/images.js
@@ -0,0 +1,71 @@
+'use strict'
+const {Storage} = require('@google-cloud/storage')
+const fs = require('fs')
+const { nanoid } = require("nanoid");
+
+const path = require('path');
+
+const pathKey = path.resolve('./serviceaccountkey.json')
+
+// TODO: Sesuaikan konfigurasi Storage
+const gcs = new Storage({
+    projectId: process.env.PROJECT_ID,
+    keyFilename: pathKey
+})
+
+// TODO: Tambahkan nama bucket yang digunakan
+const bucketName = process.env.BUCKET_NAME
+const bucket = gcs.bucket(bucketName)
+
+function getPublicUrl(filename) {
+    return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+}
+
+
+
+exports.uploadToGcs = (req, res, next) => {
+
+    if (!req.file) return next()
+
+    const gcsname = nanoid(8);
+    const file = bucket.file(gcsname)
+
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
+        }
+    })
+
+    stream.on('error', (err) => {
+        req.file.cloudStorageError = err
+        next(err)
+    })
+
+    stream.on('finish', () => {
+        req.file.cloudStorageObject = gcsname
+        req.file.cloudStoragePublicUrl = getPublicUrl(gcsname)
+
+        res.send(req.file.cloudStoragePublicUrl)
+    })
+
+    stream.end(req.file.buffer)
+   
+}
+
+exports.viewImgSpecific = (req, res, next) => {
+    const file = req.params.imageid
+    var stream = bucket.file(file).createReadStream()
+    stream.on('data', function (data) {
+        res.write(data);
+      });
+    
+      stream.on('error', function (err) {
+        console.log('error reading stream', err);
+      });
+    
+      stream.on('end', function () {
+        res.end();
+      });
+
+}
+
