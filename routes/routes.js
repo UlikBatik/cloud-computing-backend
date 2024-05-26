330e6e9cf6a9a1532ac85fd0a04b707a6cabdeae
rayhaaanm <muzakomrayhan@gmail.com>
2024-05-27 00:28:04 +0700

    add: test route for images upload and view

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const accessValidation = require("../middleware/accessValidation");
 const postController = require("../controller/postController");
-
+const Multer = require('multer');
+const images = require('../modules/images');
+const multer = Multer({
+    storage: Multer.MemoryStorage,
+    fileSize: 5 * 1024 * 1024
+})
 router.get("/", (req, res) => {
     res.status(200).json({
         "message": "Welcome to UlikBatik API server. We recommend that you first register and login before accessing our endpoints."
@@ -17,5 +22,8 @@ router.get('/users', accessValidation, auth.getUsers )
 
 router.get('/posts', accessValidation, postController.getPosts)
 router.get('/posts/:userId', accessValidation, postController.getPostsByUser)
+// router.post("/uploadImage", multer.single('image'), images.uploadToGcs)
+// router.get("/getimage/:imageid",  images.viewImgSpecific)
+
 
 module.exports = router;
