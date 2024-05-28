59521cb032f32a499a574eae7327e1f7ab9734dc
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-05-28 14:48:58 +0700

    add: batik and post routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,27 +3,40 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const accessValidation = require("../middleware/accessValidation");
 const postController = require("../controller/postController");
+const images = require("../modules/images");
 const Multer = require('multer');
-const images = require('../modules/images');
 const multer = Multer({
     storage: Multer.MemoryStorage,
-    fileSize: 5 * 1024 * 1024
+    fileSize: 3 * 1024 * 1024
 })
+
+
 router.get("/", (req, res) => {
     res.status(200).json({
         "message": "Welcome to UlikBatik API server. We recommend that you first register and login before accessing our endpoints."
     })
 });
 
-
+// Auth routes
 router.post('/register', auth.register)
 router.post('/login', auth.login)
 router.get('/users', accessValidation, auth.getUsers )
 
-router.get('/posts', accessValidation, postController.getPosts)
-router.get('/posts/:userId', accessValidation, postController.getPostsByUser)
 // router.post("/uploadImage", multer.single('image'), images.uploadToGcs)
 // router.get("/getimage/:imageid",  images.viewImgSpecific)
+
+// Post routes
+router.get('/posts', accessValidation, postController.getPosts)
+router.get('/posts/:userId', accessValidation, postController.getPostsByUser)
+router.post('/posts', accessValidation, multer.single('IMAGE'), images.uploadToGcs, postController.createPost)
+router.get('/post/:postId', accessValidation, postController.getPostById)
+router.get('/posts/:batikId', accessValidation, postController.getPostsByBatikId)
+router.delete('/post/:postId', accessValidation, postController.deletePost)
+
+// Batik routes
+router.get('/batiks', accessValidation, postController.getBatiks)
+router.get('/batik/:batikId', accessValidation, postController.getBatikById)
+//router.get('/batiks/:query', accessValidation, postController.queryBatik)
 
 
 module.exports = router;
