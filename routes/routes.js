063cd339b00cfe82f6a15596a23cbe6a59abd820
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-05-31 23:13:03 +0700

    fix: route new feat

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,21 @@
+//Middlewares
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const accessValidation = require("../middleware/accessValidation");
-const postController = require("../controller/postController");
-const batikController = require("../controller/batikController");
-const scrapController = require("../controller/scrapController");
 const images = require("../modules/images");
 const Multer = require('multer');
 const multer = Multer({
     storage: Multer.MemoryStorage,
     fileSize: 3 * 1024 * 1024
 })
+
+//Controllers
+const userController = require("../controller/userController");
+const postController = require("../controller/postController");
+const batikController = require("../controller/batikController");
+const scrapController = require("../controller/scrapController");
+const likeController = require("../controller/likeController");
 
 
 router.get("/", (req, res) => {
@@ -22,10 +27,6 @@ router.get("/", (req, res) => {
 // Auth routes
 router.post('/register', auth.register)
 router.post('/login', auth.login)
-router.get('/users', accessValidation, auth.getUsers )
-
-// router.post("/uploadImage", multer.single('image'), images.uploadToGcs)
-// router.get("/getimage/:imageid",  images.viewImgSpecific)
 
 // Post routes
 router.get('/posts', accessValidation, postController.getPosts)
@@ -36,14 +37,20 @@ router.get('/posts/:batikId', accessValidation, postController.getPostsByBatikId
 router.delete('/post/:postId', accessValidation, postController.deletePost)
 
 // Like routes
+router.get('/likes/:userId', accessValidation, likeController.getLikesByUser)
 
 // Batik routes
 router.get('/batiks', accessValidation, batikController.getBatiks)
 router.get('/batik/:batikId', accessValidation, batikController.getBatikById)
 // router.get('/batiks/:query', accessValidation, postController.queryBatik)
 
-// Matahari
+// Matahari routes
 router.get("/search/:query", accessValidation, scrapController.makeScarp);
+
+// User routes
+router.put('/user/:userId', accessValidation, multer.single('IMAGE'), images.uploadToGcs, userController.updateProfile)
+router.get('/users', accessValidation, auth.getUsers )
+router.get('/user/:userId', accessValidation, userController.getProfile)
 
 
 module.exports = router;
