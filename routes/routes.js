554b220a907ddf4580051ede7b78cb1efb0bfbbe
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-06-02 15:32:22 +0700

    feat: Add queryBatik endpoint for searching batiks by name or description

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-//Middlewares
+// Middlewares
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
@@ -7,17 +7,20 @@ const images = require("../modules/images");
 const Multer = require('multer');
 const multer = Multer({
     storage: Multer.MemoryStorage,
-    fileSize: 3 * 1024 * 1024
+    fileSize: 3 * 1024 * 1024,
+    limits: {
+        fileSize: 3 * 1024 * 1024
+    }
 })
 
-//Controllers
+// Controllers
 const userController = require("../controller/userController");
 const postController = require("../controller/postController");
 const batikController = require("../controller/batikController");
 const scrapController = require("../controller/scrapController");
 const likeController = require("../controller/likeController");
 
-
+// Welcome route
 router.get("/", (req, res) => {
     res.status(200).json({
         "message": "Welcome to UlikBatik API server. We recommend that you first register and login before accessing our endpoints."
@@ -30,7 +33,7 @@ router.post('/login', auth.login)
 
 // Post routes
 router.get('/posts', accessValidation, postController.getPosts)
-router.get('/posts/:userId', accessValidation, postController.getPostsByUser)
+//router.get('/posts/:userId', accessValidation, postController.getPostsByUser) // ini gaperlu dah dapet dari user
 router.post('/posts', accessValidation, multer.single('IMAGE'), images.uploadToGcs, postController.createPost)
 router.get('/post/:postId', accessValidation, postController.getPostById)
 router.get('/posts/:batikId', accessValidation, postController.getPostsByBatikId)
@@ -38,11 +41,12 @@ router.delete('/post/:postId', accessValidation, postController.deletePost)
 
 // Like routes
 router.get('/likes/:userId', accessValidation, likeController.getLikesByUser)
+router.post('/like/:userId', accessValidation, likeController.likePost)
 
 // Batik routes
 router.get('/batiks', accessValidation, batikController.getBatiks)
 router.get('/batik/:batikId', accessValidation, batikController.getBatikById)
-// router.get('/batiks/:query', accessValidation, postController.queryBatik)
+router.get('/batiks/:query', accessValidation, batikController.queryBatik)
 
 // Matahari routes
 router.get("/search/:query", accessValidation, scrapController.makeScarp);
