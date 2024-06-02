600b950cb3b3ca7c876617a4d78517984c91ff20
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-06-02 15:29:47 +0700

    feat: like posts

--- controller/likeController.js
+++ controller/likeController.js
@@ -21,3 +21,75 @@ exports.getLikesByUser = async (req, res) => {
         })
     }
 }
+
+exports.likePost = async (req, res) => {
+    var POSTID = req.body.POSTID;
+    const USERID = req.params.userId;
+
+    console.log(USERID)
+    console.log(POSTID)
+
+    const checkLike = await prisma.likes.count({
+        where: {
+                USERID: USERID,
+                POSTID: POSTID
+            }
+    })
+
+    if (checkLike === 1) {
+
+        await prisma.likes.delete({
+            where: {
+                USERID_POSTID: {
+                    USERID: USERID,
+                    POSTID: POSTID
+                
+                }
+            }
+        })
+
+        await prisma.post.update({
+            where: {
+                POSTID: POSTID
+            },
+            data: {
+                LIKES: {
+                    decrement: 1
+                }
+            }
+        })
+        res.status(200).json({
+            "status": true,
+            "message": "Post unliked successfully",
+        })
+    } else {
+
+    try {
+        await prisma.likes.create({
+            data: {
+                USERID: USERID,
+                POSTID: POSTID
+            }
+        })
+
+        await prisma.post.update({
+            where: {
+                POSTID: POSTID
+            },
+            data: {
+                LIKES: {
+                    increment: 1
+                }
+            }
+        })
+        res.status(200).json({
+            "status": true,
+            "message": "Post liked successfully",
+        })
+    } catch {
+        res.status(500).json({
+            "status": false,
+            "message": "An unexpected error occurred on the server",
+        })
+    }}
+}
