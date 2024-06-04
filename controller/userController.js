f8308c8890759798621217f1f41fe808b69aa195
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-06-04 17:07:10 +0700

    fix: bug fix update user

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,4 @@
+const { profile } = require("@tensorflow/tfjs-node")
 const prisma = require("../prisma/prisma")
 
 exports.getProfile = async (req, res) => {
@@ -34,19 +35,26 @@ exports.updateProfile = async (req, res) => {
     var USERNAME = req.body.USERNAME
     const USERID = req.params.userId
 
-    
+    const user = await prisma.user.findUnique({
+        where: {
+            USERID: USERID
+        },
+    })
 
-    if (!req.file){
-        imageUrl = 'url'
-    } else if (req.file && req.file.cloudStoragePublicUrl){
-        imageUrl = req.file.cloudStoragePublicUrl
+    if (!user){
+        return res.status(404).json({
+            "status": false,
+            "message": "User not found",
+        })
     }
 
-   
+    if(req.file && req.file.cloudStoragePublicUrl){
+        imageUrl = req.file.cloudStoragePublicUrl
+    } else {
+        imageUrl = user.PROFILEIMG
+    }
 
     try{
-        
-
         const updatedUser = await prisma.user.update({
             where: {
                 USERID: USERID
